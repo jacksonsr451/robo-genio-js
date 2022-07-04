5f7d1909b2991c1f70e8428fa5daa9ef01e247f0
Jackson Severino da Rocha <jackson.severino.rocha@escola.pr.gov.br>
2022-07-04 14:39:44 -0300

    Add testo no balão

--- sketch.js
+++ sketch.js
@@ -16,4 +16,11 @@ function draw() {
   background(background_img);
   image(robo, 220, 200, 150, 150);
   image(balao, 140, 10, 300, 200)
+  textSize(15);
+  text("Seja bem vindo ao Alura Gênios!" +
+      "Para adivinhar o seu nome, responda \n" +
+      "minhas perguntas com frazes \n" +
+      "longas. \n" +
+      "Clique na tela para começar!" 
+      , 180, 50)
 }
