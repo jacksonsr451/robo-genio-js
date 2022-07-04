6d8500e7959485fed49d6ce12b04094f1fca661c
Jackson Severino da Rocha <jackson.severino.rocha@escola.pr.gov.br>
2022-07-04 15:10:57 -0300

    Adcionando função para desenhar balão

--- sketch.js
+++ sketch.js
@@ -2,6 +2,8 @@ let background_img;
 let robo;
 let balao;
 
+let numero_da_pergunta = 0;
+
 function preload() {
   background_img = loadImage("img/background.png");
   robo = loadImage("img/robo.png");
@@ -15,18 +17,29 @@ function setup() {
 function draw() {
   background(background_img);
   image(robo, 220, 200, 150, 150);
+  desenharBalao(
+    "Seja bem vindo ao Alura Gênios! \n" +
+    "Para adivinhar o seu nome, responda \n" +
+    "minhas perguntas com frazes \n" +
+    "longas. \n" +
+    "Clique na tela para começar!"
+  )
+}
+
+function mousePressed() {
+  iniciarPerguntas();
+}
+
+function iniciarPerguntas() {
+  if(numero_da_pergunta == 0) {
+    
+  }
+}
+
+function desenharBalao(testoParaBalao) {
   image(balao, 120, 10, 350, 200)
   textSize(15);
   textAlign(CENTER);
   textStyle(BOLD);
-  text("Seja bem vindo ao Alura Gênios! \n" +
-      "Para adivinhar o seu nome, responda \n" +
-      "minhas perguntas com frazes \n" +
-      "longas. \n" +
-      "Clique na tela para começar!" 
-      , 290, 50)
-}
-
-function mousePressed() {
-  console.log("Teste de clique");
+  text( testoParaBalao, 290, 50)
 }
